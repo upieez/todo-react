9ff2b606e508b02df95dd5db0a1745f0175a6f4c
upieez <samuelhww@gmail.com>
2020-02-28 09:26:17 +0800

    done till mvp

--- src/client/form.jsx
+++ src/client/form.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 
 class Form extends React.Component {
-constructor(){
-    super()
-    this.state = {
-        word : ""
+    constructor(){
+        super()
+        this.state = {
+            word : "",
+            store: []
+        }
     }
-}
 
-changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
-}
+    changeHandler(event){
+        this.setState({word:event.target.value});
+    }
+
+    handleClick(){
+        this.state.store.push(this.state.word);
+        this.setState({store:this.state.store});
+    }
+
     render() {
+        const todoArr = this.state.store;
+        let todo;
+        console.log(this.state);
+        if (todoArr.length > 0) {
+          todo = this.state.store.map((todo, index) => {
+            return <li key={index}>{todo}</li>;
+          });
+        }
+
         return (
         <div className="item">
-            {this.state.word}
             <input onChange={(event)=>{this.changeHandler(event);}}/>
+            <button onClick={(event)=>{this.handleClick()}}>click me!</button>
+            <br/>
+            <ul>
+            {todo}
+            </ul>
         </div>
         );
     }
